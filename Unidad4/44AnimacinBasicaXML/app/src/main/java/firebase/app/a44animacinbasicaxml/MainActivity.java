bd20eb84627151c0061c203b2a7786f0083eb703
Sergio <selopezro@ittepic.edu.mx>
2019-05-08 22:39:21 -0500

    Animacion Basica de XML

---
+++ Unidad4/44AnimacinBasicaXML/app/src/main/java/firebase/app/a44animacinbasicaxml/MainActivity.java
@@ -0,0 +1,139 @@
+package firebase.app.a44animacinbasicaxml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.faceIcon);
+    }
+
+    public void onButtonClick(View v) {
+        Animation animation =
+                AnimationUtils.loadAnimation(this, R.anim.grow);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation);
+    }
+    public void onButtonClick2(View v) {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.alpha);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation);
+    }
+    public void onButtonClick3(View v) {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rebotar);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation);
+    }
+    public void onButtonClick4(View v) {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.parpadeo);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation);
+    }
+    public void onButtonClick5(View v) {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.ambos);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation);
+    }
+    public void onButtonClick6(View v) {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fundirse);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation);
+    }
+}

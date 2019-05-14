3949233b85db279e2e6ebff254f464c917194054
Sergio <selopezro@ittepic.edu.mx>
2019-05-13 19:54:21 -0500

    Contador de pasos

---
+++ Unidad5/U5_ContadorPasos-master/app/src/main/java/mx/edu/ittepic/u5_contadorpasos/SimplePedometerActivity.java
@@ -0,0 +1,75 @@
+package mx.edu.ittepic.u5_contadorpasos;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SimplePedometerActivity extends AppCompatActivity implements SensorEventListener, StepListener {
+
+    private TextView textView;
+    private SimpleStepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private static final String TEXT_NUM_STEPS = "Numero de pasos: ";
+    private int numSteps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //textView = (TextView) findViewById(R.id.textView);
+        textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setTextColor(Color.RED);
+        textView.setGravity(1);
+        setContentView(textView);
+        setTitle("Contador de pasos");
+
+        // Get an instance of the SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new SimpleStepDetector();
+        simpleStepDetector.registerListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        numSteps = 0;
+        textView.setText(TEXT_NUM_STEPS + numSteps);
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
+    }
+
+    @Override
+    public void step(long timeNs) {
+        numSteps++;
+        textView.setText(TEXT_NUM_STEPS + numSteps);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
---
+++ Unidad5/U5_ContadorPasos-master/app/src/main/java/mx/edu/ittepic/u5_contadorpasos/StepListener.java
@@ -0,0 +1,9 @@
+package mx.edu.ittepic.u5_contadorpasos;
+
+/**
+ * Created by Zulma on 17/05/2018.
+ */
+
+public  interface StepListener {
+    public  void  step(long timeNs);
+}

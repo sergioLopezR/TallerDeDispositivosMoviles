9593d17fd7874cdc4af1e0005c540852106b4982
Sergio <selopezro@ittepic.edu.mx>
2019-05-20 20:13:47 -0500

    Proveedor de Contenidos

---
+++ Unidad5/55ProveedordeContenidos/app/src/main/java/firebase/app/a55proveedordecontenidos/Llamada.java
@@ -0,0 +1,37 @@
+package firebase.app.a55proveedordecontenidos;
+
+public class Llamada {
+    private  String id;
+    private String tipo;
+    private String telefono;
+
+    public Llamada(String id, String tipo, String telefono) {
+        this.id = id;
+        this.tipo = tipo;
+        this.telefono = telefono;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
---
+++ Unidad5/55ProveedordeContenidos/app/src/main/java/firebase/app/a55proveedordecontenidos/MainActivity.java
@@ -0,0 +1,98 @@
+package firebase.app.a55proveedordecontenidos;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CallLog;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtResultados;
+    DatabaseReference myRef;
+    int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myRef = FirebaseDatabase.getInstance().getReference("llamada");
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.INTERNET)) {
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.INTERNET}, MY_PERMISSIONS_REQUEST_READ_CONTACTS); }
+        }
+        txtResultados = findViewById(R.id.salida);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.READ_CALL_LOG)) {
+            } else {
+
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.READ_CALL_LOG}, MY_PERMISSIONS_REQUEST_READ_CONTACTS); }
+        }
+
+        String[] projection = new String[]{
+                CallLog.Calls.TYPE,
+                CallLog.Calls.NUMBER};
+
+        Uri llamadasUri = CallLog.Calls.CONTENT_URI;
+
+        ContentResolver cr = getContentResolver();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            return;
+        }
+        Cursor cur = cr.query(llamadasUri, projection, null, null, null);
+
+        if (cur.moveToFirst())
+        {
+            int tipo;
+            String tipoLlamada = "";
+            String telefono;
+
+            int colTipo = cur.getColumnIndex(CallLog.Calls.TYPE);
+            int colTelefono = cur.getColumnIndex(CallLog.Calls.NUMBER);
+
+            txtResultados.setText("");
+
+            do
+            {
+                tipo = cur.getInt(colTipo);
+                telefono = cur.getString(colTelefono);
+
+                if(tipo == CallLog.Calls.INCOMING_TYPE)
+                    tipoLlamada = "ENTRADA";
+                else if(tipo == CallLog.Calls.OUTGOING_TYPE)
+                    tipoLlamada = "SALIDA";
+                else if(tipo == CallLog.Calls.MISSED_TYPE)
+                    tipoLlamada = "PERDIDA";
+
+                addCallRegister(tipoLlamada,telefono);
+                txtResultados.append(tipoLlamada + " --->  " + telefono + "\n");
+
+            } while (cur.moveToNext());
+        }
+
+    }
+
+    public void addCallRegister(String tipo, String telefono){
+        if(!TextUtils.isEmpty(telefono)){
+            String id = myRef.push().getKey();
+            Llamada call = new Llamada(id, tipo, telefono);
+            myRef.child(id).setValue(call);
+        }
+
+    }
+}
